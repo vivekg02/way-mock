5f7bd74dd320cb143c9f303193111e717bfc240d
Vickygulajkar <vickygulajkar@gmail.com>
2024-11-09 15:48:16 +0530

    changes for deployment

--- backend.js
+++ backend.js
@@ -1,27 +1,18 @@
 const express = require('express');
-const fetch = require('node-fetch'); // Replace request with fetch
+const request = require('request');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.get('/proxy', async (req, res) => {
+app.get('/proxy', (req, res) => {
   const url = req.query.url;
   if (url) {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        response.body.pipe(res);
-      } else {
-        res.status(400).send('Error fetching the URL');
-      }
-    } catch (error) {
-      res.status(500).send('Error fetching the URL');
-    }
+    request(url).pipe(res);
   } else {
     res.status(400).send('URL parameter is missing');
   }
