b1230f4e431704a08423d168535efac7abb0f914
Vickygulajkar <vickygulajkar@gmail.com>
2024-11-09 15:43:56 +0530

    changes

--- backend.js
+++ backend.js
@@ -1,18 +1,27 @@
 const express = require('express');
-const request = require('request');
+const fetch = require('node-fetch'); // Replace request with fetch
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.get('/proxy', (req, res) => {
+app.get('/proxy', async (req, res) => {
   const url = req.query.url;
   if (url) {
-    request(url).pipe(res);
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        response.body.pipe(res);
+      } else {
+        res.status(400).send('Error fetching the URL');
+      }
+    } catch (error) {
+      res.status(500).send('Error fetching the URL');
+    }
   } else {
     res.status(400).send('URL parameter is missing');
   }
